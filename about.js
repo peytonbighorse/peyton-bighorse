9443f99b1a9f5c65e570f9bbb4986e2dae39ff25
pmb9308613 <162384210+pmb9308613@users.noreply.github.com>
2025-06-30 19:51:48 -0700

    Added projects and contact pages

--- about.js
+++ about.js
@@ -16,10 +16,17 @@ function displayCurrentSection(event) {
     currentSection.innerHTML = `<div>
     <a href="#" class="current-button">Reading now: The Letters of Vincent Van Gogh by Mark Roskill</a>
     <a href="#" class="current-button">Recently finished: Run For The Hills by Kevin Wilson</a>
-    <a href="#" class="current-button">Gumshoe - Samantha Crain</a>
-    <a href="#" class="current-button">Pod Save the World</a>
-    <a href="#" class="current-button">Name of the Wind - Patrick Rothfuss</a>
+    <a href="#" class="current-button">What's next: </a>
+    <a href="#" class="current-button">My favorite:</a>
+    </div>`;
+  }
+  if (currentValue === "watching") {
+    currentSection.innerHTML = `<div>
+    <a href="#" class="current-button">Recently finished: It's Not Yet Dark</a>
+    <a href="#" class="current-button">What's next: All About Lily Chou-Chou</a>
+    <a href="#" class="current-button">My favorite: The Banshees of Inisherin</a>
     </div>`;
   }
 }
+
 currentSelector.addEventListener("change", displayCurrentSection);

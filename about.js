159bd8b022ef9282c1589f11866fdfe06c0e4390
Peyton Bighorse <162384210+peytonbighorse@users.noreply.github.com>
2026-07-06 16:27:25 -0700

    Updated about and home pages. Added TourTopia to projects.

--- about.js
+++ about.js
@@ -16,22 +16,22 @@ function displayCurrentSection(event) {
   }
   if (currentValue === "reading") {
     currentSection.innerHTML = `<div>
-    <a href="https://www.goodreads.com/book/show/35036409-my-brilliant-friend?ref=nav_sb_ss_1_19" class="current-button" target="_blank">Reading now: My Brilliant Friend - Elena Ferrante</a>
-    <a href="https://www.goodreads.com/book/show/197798168-all-fours?ref=nav_sb_ss_1_9" class="current-button" target="_blank">Recently finished: All Fours - Miranda July</a>
+    <a href="https://www.goodreads.com/en/book/show/214152261-flesh" class="current-button" target="_blank">Reading now: Flesh - David Szalay</a>
+    <a href="https://www.goodreads.com/book/show/37800111-the-last-children-of-tokyo?from_search=true&from_srp=true&qid=kc1TlB9uvs&rank=2" class="current-button" target="_blank">Recently finished: The Emmisary - Yoko Tawada</a>
     <a href="https://www.goodreads.com/book/show/62919375-shark-heart?ref=nav_sb_ss_1_11" class="current-button" target="_blank">What's next: Shark Heart - Emily Habeck</a>
-    <a href="https://www.goodreads.com/book/show/58784475-tomorrow-and-tomorrow-and-tomorrow?ref=nav_sb_ss_1_10" class="current-button" target="_blank">All-time favorite: Tomorrow, Tomorrow, and Tomorrow by Gabrielle Zevin</a>`
-    /*<a href="https://www.goodreads.com/book/show/174147294-wandering-stars?ref=nav_sb_ss_1_15" class="current-button" target="_blank">Favorite this year (so far): Wandering Stars by Tommy Orange</a>
-    </div>`*/;
+    <a href="https://www.goodreads.com/book/show/58784475-tomorrow-and-tomorrow-and-tomorrow?ref=nav_sb_ss_1_10" class="current-button" target="_blank">All-time favorite: Tomorrow, Tomorrow, and Tomorrow by Gabrielle Zevin</a>
+    <a href="https://www.goodreads.com/book/show/25242224-the-story-of-the-lost-child?ref=nav_sb_ss_1_23" class="current-button" target="_blank">Recently loved: The Story of a Lost Child - Elena Ferrante</a>`
+      ;
   }
   if (currentValue === "watching") {
     currentSection.innerHTML = `<div>
-    <a href="https://ebird.org/species/perfal/" class="current-button" target="_blank">Recently loved: Peregrine Falcon</a>
-    <a href="https://ebird.org/species/phaino" class="current-button" target="_blank">Looking for: Phainopepla</a>
+    <a href="https://ebird.org/species/pitwhy" class="current-button" target="_blank">Recently loved: Pin-tailed Wydah</a>
+    <a href="https://ebird.org/species/verfly" class="current-button" target="_blank">Looking for: Vermillion Flycatcher</a>
     <a href="https://ebird.org/species/stejay/" class="current-button" target="_blank">My #1 Favorite: Steller's Jay</a>
     <a href="https://ebird.org/species/grbher3/" class="current-button" target="_blank">Georgie's Favorite: 
     Great Blue Heron</a>
-    <a href="https://ebird.org/species/greroa/" class="current-button" target="_blank">Favorite of the Year (so far): 
-    Greater Roadrunner</a>
+    <a href="https://ebird.org/species/phaino" class="current-button" target="_blank">Favorite of the Year (so far): 
+    Phainopepla</a>
     </div>`;
   }
 }
